f3ec87627f0c338a3799781f38e64ebfc45f6a59
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-12-22 15:16:56 +0100

    Add project files.

---
+++ microsoftLearnExercise27/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime;
+
+namespace microsoftLearnExercise27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Console.WriteLine("Would you like to play? (Y/N)");
+            if (ShouldPlay())
+            {
+                PlayGame();
+            }
+
+            void PlayGame()
+            {
+                var play = true;
+
+                while (play)
+                {
+                    var target = random.Next(1, 5 );
+                    var roll = random.Next(1, 6);
+
+                    Console.WriteLine($"\nRoll a number greater than {target} to win!");
+                    Console.WriteLine($"You rolled a {roll}");
+                    Console.WriteLine(WinOrLose(target, roll));
+                    Console.WriteLine("\nPlay again? (Y/N)");
+
+                    play = ShouldPlay();
+                }
+            }
+
+            bool ShouldPlay()
+            {
+                char userInput = Console.ReadKey().KeyChar;
+
+                return userInput == 'y' ? true : false;
+            }
+
+            string WinOrLose(int target, int roll)
+            {
+                if (roll > target)
+                    return "You win!";
+
+                return "You lose!";
+            }
+        }
+    }
+}
